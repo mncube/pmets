cef005dbcc8ed44bfa60a0c16764fafbc65c689d
Mackson Ncube <macksonncube@gmail.com>
2022-09-28 11:11:56 -0700

    additional work on item_pool functions

--- R/item_pool.R
+++ R/item_pool.R
@@ -1,29 +1,30 @@
-item_pool <- function(df,
-                      L1N,
-                      L1W,
-                      L1F = function(x)x,
-                      L2N = NULL,
-                      L2F = NULL,
-                      L3N = NULL,
-                      L3F = NULL,
-                      cal_orig = 20,
-                      min_tl = 4,
+item_pool_1l <- function(df,
+                      l1_name,
+                      l1_func = NULL,
+                      cal_length = 20,
+                      min_l1 = 4,
                       forms = 2,
                       mults = 3.3,
                       max_its = 15,
                       seed = 100){
-  #Initialize repeat loop
-  df <- df %>% dplyr::mutate(cal = cal_orig)
-  its <- 1
-  it_rounds <- 1
+  #Set Seed
+  set.seed(seed)
 
-  if (is.null(L2N) & is.null(L3N)){
+  #Initialize repeat loop
+  cal <- cal_length
+  its <- 1
+  it_round <- 1
+
+  #Check that a level 1 variable name has been provided
+  if (missing(l1_name)){
+    stop("Must provide a level 1 variable name", call. = TRUE)
+  }
+
     repeat{
 
       df2 <- df %>%
-        dplyr::mutate(cal = cal) %>%
         #mutate(Items_UL1 = round(cal*.25*(W_FOU + W_IOPS + W_IONC + W_DEO), 0)) %>%
-        dplyr::mutate(L1_Items = L1F(x)) %>%
+        dplyr::mutate(l1_items = do.call(l1_func, lapply(formalArgs(l1_func), as.name))) %>%
         #group_by(Domain) %>%
         #mutate(Domain_Length = n())%>%
         #mutate(Items_D = sum(Items_R)) %>%
@@ -34,41 +35,44 @@ item_pool <- function(df,
         #mutate(Items_R = ifelse(Items_D < min_items_per_domain,
         #                        round(Items_R + (min_items_per_domain - Items_D)/Domain_Length,
         #                              0), Items_R)) %>%
-        dplyr::mutate(L1_Items = ifelse(L1_Items < min_tl,
-                                round(L1_Items + (min_tl - L1_Items), 0),
-                                L1_Items))
+        dplyr::mutate(l1_items = ifelse(l1_items < min_l1,
+                                round(l1_items + (min_l1 - l1_items), 0),
+                                l1_items))
 
       #Check that the calibration number worked
-      if (sum(df$L1_Items) %in% c(cal_orig - 1, cal_orig, cal_orig + 1)) {break}
+      if (sum(df$l1_items) %in% c(cal_length - 1, cal_length, cal_length + 1)) {break}
 
       if (its == max_its){stop("algorithm didn't converge")}
 
       #Update cal and its
-      df <- df %>%
-        dplyr::mutate(cal = cal - 1)
+      cal <- cal - 1
       its <- its + 1
     }
 
     #Randomly add item if total length cal - 1
-    if (sum(df$L1_Items) == cal_orig - 1){
+    if (sum(df$l1_items) == cal_length - 1){
       tot_rows <- nrow(df)
       df <- df %>%
         mutate(fix_length_random_order = sample(1:tot_rows, tot_rows, replace = FALSE)) %>%
-        mutate(L1_Items = ifelse(fix_length_random_order == 1, L1_Items + 1, L1_Items)) %>%
-        mutate(L1_Items = max(L1_Items)) %>%
+        mutate(l1_items = ifelse(fix_length_random_order == 1, l1_items + 1, l1_items)) %>%
+        mutate(l1_items = max(l1_items)) %>%
         ungroup()
     }
 
     #Randomly subtract item if total length is cal + 1
-    if (sum(df$L1_Items) == cal_orig + 1){
+    if (sum(df$l1_items) == cal_length + 1){
       tot_rows <- nrow(df)
       df <- df%>%
         mutate(fix_length_random_order = sample(1:tot_rows, tot_rows, replace = FALSE)) %>%
-        mutate(L1_Items = ifelse(fix_length_random_order == 1, L1_Items - 1, L1_Items)) %>%
-        mutate(L1_Items = min(L1_Items))
+        mutate(l1_items = ifelse(fix_length_random_order == 1, l1_items - 1, l1_items)) %>%
+        mutate(l1_items = min(l1_items))
     }
 
-  }
+  #Get number of level1 items in item bank
+  df <- df %>%
+    mutate(l1_items_bank = round(l1_items*forms*mults))
+
+
 
   return(df)
 
--- tests/testthat/test-item_pool.R
+++ tests/testthat/test-item_pool.R
@@ -1,16 +1,35 @@
-test_that("UL1 returns a dataframe of length X", {
+test_that("item_pool_1l returns a dataframe of length 4", {
 
   df_ul1 <- data.frame(domain = 1:4,
-                       imp_w = c(.25, .25, .25, .25),
-                       dif_w = c(.25, .25, .25, .25))
+                       imp_w = c(1, 1, 1, 1),
+                       dif_w = c(1, 1, 1, 1))
 
-  df_ul1_ip <- item_pool(df = df_ul1,
-                         L1N = "domain",
-                         L1W = list("imp_w", "dif_w"),
-                         L1F = function(x)mean(x),
-                         cal_orig = 20,
+  df_ul1_ip <- item_pool_1l(df = df_ul1,
+                            l1_name = "domain",
+                         l1_func = function(imp_w, dif_w)5*rowSums(cbind(imp_w,dif_w)),
+                         cal_length = 40,
                          max_its = 100)
 
 
   expect_equal(2 * 2, 4)
 })
+
+#item_pool <- function(df,
+#                      L1F,
+#                      w){
+#  #f <- function(...) names(rlang::enexprs(...))
+#  #params <- mget(formalArgs(L1F))
+#  x <- w
+#  df %>%
+#    dplyr::mutate(L1_Items = do.call(L1F, as.list(formalArgs(L1F))))
+#}
+
+#Create data frame
+#df_ul1 <- data.frame(domain = 1:4,
+#                     y = c(.25, .25, .25, .25),
+#                     z = c(.25, .25, .25, .25))
+
+#Create L1_Items variable based on L1F function
+#df_ul1_ip <- item_pool(df = df_ul1,
+#                       L1F = function(x,y,z){ x * rowMeans(cbind(y, z)) },
+#                       w = 20)
